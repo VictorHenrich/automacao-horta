59bde3ecfb92b478bbb26c74f2aa71a45a3f40a8
VictorHenrich <victorhenrich993@gmail.com>
2025-01-15 15:36:39 -0300

    Removendo a pasta testes

--- tests/test_infrared_sensor.py
+++
@@ -1,8 +0,0 @@
-from services.infrared_sensor import InfraredSensorService
-from utils import config
-
-
-infrared_sensor_service = InfraredSensorService(port=config.INFRARED_SENSOR_PORT)
-
-while True:
-    infrared_sensor_service.execute()
