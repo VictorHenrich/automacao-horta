8297024c9186918708fae8513001ec8ea9fffff2
VictorHenrich <victorhenrich993@gmail.com>
2025-01-30 10:38:51 -0300

    Adicionando melhorias

--- utils/pins.py
+++ utils/pins.py
@@ -1,6 +1,16 @@
 from machine import Pin, ADC
 
 
+class PinAttenuityTypes:
+    ATTN_11DB = ADC.ATTN_11DB
+
+    ATTN_2_5DB = ADC.ATTN_2_5DB
+
+    ATTN_0DB = ADC.ATTN_0DB
+
+    ATTN_6DB = ADC.ATTN_6DB
+
+
 class PinTypes:
     IN = Pin.IN
 
@@ -18,4 +28,4 @@ class AnalogPin(ADC):
     def __init__(self, port, type=PinTypes.IN, **kwargs):
         pin = Pin(port, type)
 
-        super().__init__(pin, atten=ADC.ATTN_11DB, **kwargs)
+        super().__init__(pin, atten=PinAttenuityTypes.ATTN_11DB, **kwargs)
